ac9c80fdca8b77a5757703cb371629738cce0b24
tujinqiu <tujinqiu@tujinqiudeiMac.local>
2015-05-13 15:47:50 +0800

    Create the HustLibrary project

---
+++ HustLibrary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HustLibrary
+//
+//  Created by tujinqiu on 15/5/13.
+//  Copyright (c) 2015年 tujinqiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HustLibrary/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HustLibrary
+//
+//  Created by tujinqiu on 15/5/13.
+//  Copyright (c) 2015年 tujinqiu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
